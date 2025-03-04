8a0803bca3d6da2a727f24e8c071b61ccbb6906b
Ismail Mazumder <im087921@gmail.com>
2025-03-04 16:04:33 +0600

    hhh

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,34 @@
+#include <jni.h>
+#include <string>
+#include<stdio.h>
+#include <stdlib.h>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <memory>
+
+std::string list()
+{
+    std::string total_txt;
+    char buffer[121] = {0};  // ইনিশিয়ালাইজ করা হলো
+    FILE *fp = popen("ls ", "r");
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
+        total_txt = total_txt + buffer;
+    }
+    return total_txt;
+}
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myapplication_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string final = list();
+    return env->NewStringUTF(final.c_str());
+}
+
+int main()
+{
+
+
+    return 0;
+}
